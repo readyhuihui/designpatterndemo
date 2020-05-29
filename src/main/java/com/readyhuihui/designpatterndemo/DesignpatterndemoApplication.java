412ccbd97331c128acd900aed7b0d24a3f870e36
liyonghui <liyonghui@cdvcloud.com>
2020-05-29 23:48:12 +0800

    设计模式示例项目 Initial commit

---
+++ src/main/java/com/readyhuihui/designpatterndemo/DesignpatterndemoApplication.java
@@ -0,0 +1,13 @@
+package com.readyhuihui.designpatterndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatterndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatterndemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/readyhuihui/designpatterndemo/DesignpatterndemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readyhuihui.designpatterndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatterndemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
