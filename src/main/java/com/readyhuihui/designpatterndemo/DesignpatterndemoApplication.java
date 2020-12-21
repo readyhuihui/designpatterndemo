b05f8f6faed071d1eed6dbfef44fd00827711422
liyonghui <liyonghui@cdvcloud.com>
2020-12-21 15:01:09 +0800

    测试推送备注

--- src/main/java/com/readyhuihui/designpatterndemo/DesignpatterndemoApplication.java
+++ src/main/java/com/readyhuihui/designpatterndemo/DesignpatterndemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesignpatterndemoApplication {
-//测试
+//测试123123
     public static void main(String[] args) {
         SpringApplication.run(DesignpatterndemoApplication.class, args);
     }
